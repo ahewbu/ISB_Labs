7d455b06a9ce8debe8594e927eeeb197261afb0b
ahewbu <shlnsn@outlook.com>
2024-05-03 11:11:47 +0400

    changed input parameters for tasks, took paths to directory "paths.json"

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,3 +1,6 @@
+import json
+
+
 def read_text(path: str):
     with open(path, 'r', encoding='UTF-8') as f:
         text = f.read().lower()
@@ -17,12 +20,10 @@ def decrypt(text, decryption_mapping):
     return decrypted_text
 
 
-def task_1():
-    text = \
-        read_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task1\\text.txt")
+def task_1(plain_text: str, key: str, encrypted: str):
+    text = read_text(plain_text)
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_replacement = \
-        read_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task1\\key.txt")
+    alphabet_replacement = read_text(key)
 
     print(alphabet_replacement)
 
@@ -37,32 +38,44 @@ def task_1():
 
     print(result_string)
 
-    write_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task1\\encrypt.txt", \
-               result_string)
+    write_text(encrypted, result_string)
 
     for i in range(0, len(alphabet)):
         print(alphabet[i] + ' -> ' + alphabet_replacement[i])
 
 
-def task_2():
-    crypt_alphabet = \
-        read_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task2\\key.txt")
+def task_2(plain_text: str, key: str, decrypted: str):
+    crypt_alphabet = read_text(key)
     normal_alphabet = " ОИЕНШЧЬПРЭТЙКЯМСЛДЗАЖЦУВГБФХЮ,ЩЫ."
 
     decryption_mapping = dict(zip(crypt_alphabet, normal_alphabet))
 
-    data = \
-        read_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task2\\text.txt")
+    data = read_text(plain_text)
 
     decrypted_text = decrypt(data, decryption_mapping)
 
-    write_text("D:\\SHALAN REP\\ISB_Labs\\lab_1\\texts\\task2\\result.txt", \
-               decrypted_text)
+    write_text(decrypted, decrypted_text)
 
-    
+
+def read_json(file: str):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    text_1 = data.get("path_to_text_1")
+    key_1 = data.get("path_to_key_1")
+    encrypted = data.get("path_to_encryption_1")
+
+    text_2 = data.get("path_to_text_2")
+    key_2 = data.get("path_to_key_2")
+    decrypted = data.get("path_to_decrypt_2")
+
+    return text_1, key_1, encrypted, text_2, key_2, decrypted
+
+
 def main():
-    task_1()
-    task_2()
+    text_1, key_1, encrypted, text_2, key_2, decrypted = read_json("paths.json")
+    task_1(text_1, key_1, encrypted)
+    task_2(text_2, key_2, decrypted)
+
 
 if __name__ == '__main__':
     main()
