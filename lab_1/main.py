e0dece528cf53d8512b2e89db3bafe8c81a9fdca
ahewbu <shlnsn@outlook.com>
2024-05-03 23:43:40 +0400

    Fixed errors

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,24 +1,56 @@
-import json
+from work_files import *
 
-def read_text(path: str):
-    """The function of reading text from file
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def task_1(plain_text: str, key: str, encrypted: str):
+    """Encryption by a simple transposition cipher
     Args:
-      path: the path to the file
-    Returns:
-      text from the file
+      plain_text: encrypted text
+      key: required key string
+      encrypted: string where the resulting cipher is written
     """
-    with open(path, 'r', encoding='UTF-8') as f:
-        text = f.read().lower()
-    return text
+    text = read_text(plain_text)
+    alphabet_replacement = read_text(key)
+
+    print(alphabet_replacement)
+
+    result_string = ''
+    for letter in text:
+        try:
+            index = alphabet.index(letter)
+            result_string = result_string + alphabet_replacement[index]
+        except ValueError:
+            print("That was no valid number!")
+            result_string = result_string + letter      
+
+    print(result_string)
+
+    write_text(encrypted, result_string)
+
+    for i in range(0, len(alphabet)):
+        print(alphabet[i] + ' -> ' + alphabet_replacement[i])
 
 
-def write_text(path: str, text: str):
-    """The function of writing information to file
+def task_2(plain_text: str, key: str, decrypted: str, normal_alphabet: str):
+    """The function for decryption by 
     Args:
-      path: the path to the file
+      plain_text: encrypted text
+      key: required key string
+      decrypted: string where the resulting deciphering is written
+      normal_alphabet: alphabet to decrypt
     """
-    with open(path, 'w', encoding='UTF-8') as f:
-        f.write(text)
+    crypt_alphabet = read_text(key)
+    normal_alphabet = read_text(normal_alphabet)
+
+    decryption_mapping = dict(zip(crypt_alphabet, normal_alphabet))
+
+    data = read_text(plain_text)
+
+    decrypted_text = decrypt(data, decryption_mapping)
+
+    write_text(decrypted, decrypted_text)
 
 
 def decrypt(text, decryption_mapping):
@@ -36,79 +68,11 @@ def decrypt(text, decryption_mapping):
     return decrypted_text
 
 
-def task_1(plain_text: str, key: str, encrypted: str):
-    """Encryption by a simple transposition cipher
-    Args:
-      plain_text: encrypted text
-      key: required key string
-      encrypted: string where the resulting cipher is written
-    """
-    text = read_text(plain_text)
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_replacement = read_text(key)
-
-    print(alphabet_replacement)
-
-    result_string = ''
-    for letter in text:
-        try:
-            index = alphabet.index(letter)
-        except ValueError:
-            result_string = result_string + letter
-            continue
-        result_string = result_string + alphabet_replacement[index]
-
-    print(result_string)
-
-    write_text(encrypted, result_string)
-
-    for i in range(0, len(alphabet)):
-        print(alphabet[i] + ' -> ' + alphabet_replacement[i])
-
-
-def task_2(plain_text: str, key: str, decrypted: str):
-    """The function for decryption by 
-    Args:
-      plain_text: encrypted text
-      key: required key string
-      decrypted: string where the resulting deciphering is written
-    """
-    crypt_alphabet = read_text(key)
-    normal_alphabet = " ОИЕНШЧЬПРЭТЙКЯМСЛДЗАЖЦУВГБФХЮ,ЩЫ."
-
-    decryption_mapping = dict(zip(crypt_alphabet, normal_alphabet))
-
-    data = read_text(plain_text)
-
-    decrypted_text = decrypt(data, decryption_mapping)
-
-    write_text(decrypted, decrypted_text)
-
-
-def read_json(file: str):
-    """The function of reading data from a json file
-    Args:
-      path: the path to the file
-    Returns:
-      parameters from json file
-    """
-    with open("D:\SHALAN REP\\ISB_Labs\\lab_1\\paths.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-    text_1 = data.get("path_to_text_1")
-    key_1 = data.get("path_to_key_1")
-    encrypted = data.get("path_to_encryption_1")
-
-    text_2 = data.get("path_to_text_2")
-    key_2 = data.get("path_to_key_2")
-    decrypted = data.get("path_to_decrypt_2")
-
-    return text_1, key_1, encrypted, text_2, key_2, decrypted
-
-
 def main():
-    text_1, key_1, encrypted, text_2, key_2, decrypted = read_json("paths.json")
+    text_1, key_1, encrypted, text_2, key_2, \
+        decrypted, normal_alphabet = read_json("paths.json")
     task_1(text_1, key_1, encrypted)
-    task_2(text_2, key_2, decrypted)
+    task_2(text_2, key_2, decrypted, normal_alphabet)
 
 
 if __name__ == '__main__':
