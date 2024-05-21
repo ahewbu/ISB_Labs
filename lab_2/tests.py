1e19da0704a066e2102d237e60b5c00d2c398fdc
ahewbu <shlnsn@outlook.com>
2024-05-21 19:26:10 +0400

    added 3 nist-tests in tests.py, wrote explanation of lab in report.md

--- lab_2/tests.py
+++ lab_2/tests.py
@@ -9,45 +9,45 @@ logging.basicConfig(level=logging.INFO)
 PI = {1: 0.2148, 2: 0.3672, 3: 0.2305, 4: 0.1875}
 
 
-def frequency_bit_test(sequence) -> float:
+def frequency_bit_test(seq) -> float:
     try:
         summary = 0
-        for i in sequence:
+        for i in seq:
             if int(i) == 1:
                 summary += 1
             else:
                 summary -= 1
-        summary = math.fabs(summary) / math.sqrt(len(sequence))
-        p_value = math.erfc(summary / math.sqrt(2))
-        return p_value
-    except Exception as ex:
-        logging.error(f"ZeroDivisionError: {ex.message}\n{ex.args}\n")
+        summary = math.fabs(summary) / math.sqrt(len(seq))
+        p_val = math.erfc(summary / math.sqrt(2))
+        return p_val
+    except Exception as er:
+        logging.error(f"ZeroDivisionError: {er.message}\n{er.args}\n")
 
 
-def ordinary_bit_test(sequence) -> float:
+def ordinary_bit_test(seq) -> float:
     try:
-        counter = sequence.count("1")
-        counter *= 1 / len(sequence)
-        if abs(counter - 0.5) < 2 / math.sqrt(len(sequence)):
+        cntr = seq.count("1")
+        cntr *= 1 / len(seq)
+        if abs(cntr - 0.5) < 2 / math.sqrt(len(seq)):
             v = 0
-            for i in range(len(sequence) - 1):
-                if sequence[i] != sequence[i + 1]:
+            for i in range(len(seq) - 1):
+                if seq[i] != seq[i + 1]:
                     v += 1
-            num = abs(v - 2 * len(sequence) * counter * (1 - counter))
-            denom = 2 * math.sqrt(2 * len(sequence)) * counter * (1 - counter)
-            p_value = math.erfc(num / denom)
+            num = abs(v - 2 * len(seq) * cntr * (1 - cntr))
+            denom = 2 * math.sqrt(2 * len(seq)) * cntr * (1 - cntr)
+            p_val = math.erfc(num / denom)
         else:
-            p_value = 0
-        return p_value
-    except Exception as ex:
-        logging.error(f"ZeroDivisionError: {ex.message}\n{ex.args}\n")
+            p_val = 0
+        return p_val
+    except Exception as er:
+        logging.error(f"ZeroDivisionError: {er.message}\n{er.args}\n")
 
 
-def split_bits(sequence) -> list:
+def split_bits(seq) -> list:
     blocks = []
-    quantity = len(sequence) - (len(sequence) % 8)
-    for i in range(0, quantity, 8):
-        block = sequence[i: i + 8]
+    amount = len(seq) - (len(seq) % 8)
+    for i in range(0, amount, 8):
+        block = seq[i: i + 8]
         blocks.append(block)
     return blocks
 
@@ -56,36 +56,36 @@ def largest_number_of_units(blocks: list) -> dict:
     try:
         unit_counts = {}
         for block in blocks:
-            counter = 0
-            max_counter = 0
+            cntr = 0
+            max_cntr = 0
             for i in block:
                 if int(i) == 1:
-                    counter += 1
-                    max_counter = max(max_counter, counter)
-                    if max_counter > 4:
-                        max_counter = 4
+                    cntr += 1
+                    max_cntr = max(max_cntr, cntr)
+                    if max_cntr > 4:
+                        max_cntr = 4
                 else:
-                    counter = 0
-            if max_counter in unit_counts:
-                unit_counts[max_counter] += 1
+                    cntr = 0
+            if max_cntr in unit_counts:
+                unit_counts[max_cntr] += 1
             else:
-                unit_counts[max_counter] = 1
+                unit_counts[max_cntr] = 1
         sorted_dict = dict(sorted(unit_counts.items(), key=lambda x: x[1]))
         return sorted_dict
-    except Exception as ex:
-        logging.error(f"TypeError block wasn't str: {ex.message}\n{ex.args}\n")
+    except Exception as er:
+        logging.error(f"TypeError block wasn't str: {er.message}\n{er.args}\n")
 
 
 def check_len_test(dictionary: dict) -> float:
     try:
-        square_x = 0
-        for i, value in dictionary.items():
-            square_x += pow(value - 16 * PI[i], 2) / (16 * PI[i])
-        p_value = mpmath.gammainc(3 / 2, square_x / 2)
-        return p_value
-    except Exception as ex:
+        sqr_x = 0
+        for i, val in dictionary.items():
+            sqr_x += pow(val - 16 * PI[i], 2) / (16 * PI[i])
+        p_val = mpmath.gammainc(3 / 2, sqr_x / 2)
+        return p_val
+    except Exception as er:
         logging.error(
-            f"Length of the dictionary is longer than number of pi-constants: {ex.message}\n{ex.args}\n"
+            f"Length of the dictionary is longer than number of pi-constants: {er.message}\n{er.args}\n"
         )
 
 
